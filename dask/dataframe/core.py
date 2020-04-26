cc30060d86f8c0d2a07a96202a9bd9e178daa029
Ryan Williams <ryan.blake.williams@gmail.com>
2020-04-26 23:05:01 +0000

    add missing iloc return stmt

--- dask/dataframe/core.py
+++ dask/dataframe/core.py
@@ -3400,7 +3400,7 @@ class DataFrame(_Frame):
             # Slicing with integer labels is always iloc based except for a
             # float indexer for some reason
             if is_integer_slice and not is_float_dtype(self.index.dtype):
-                self.iloc[key]
+                return self.iloc[key]
             else:
                 return self.loc[key]
 
