f57b0089c052c4440256b479bcf31ee297f7ee40
Ryan Williams <ryan@runsascoded.com>
2020-06-28 14:22:42 +0000

    improve .A, test

--- dask/array/core.py
+++ dask/array/core.py
@@ -1707,7 +1707,13 @@ class Array(DaskMethodsMixin):
 
     @property
     def A(self):
-        return self.map_blocks(lambda block: block.A, dtype=self.dtype)
+        if type(self._meta) is np.ndarray:
+            print('skipping .A')
+            return self
+        else:
+            # numpy.matrix, scipy.sparse.spmatrix, …
+            print('mapping .A')
+            return self.map_blocks(lambda block: block.A, dtype=self.dtype)
 
     @property
     def T(self):
