50446b6eccf4ad7b3cd3a7efdec7ac404ea23df5
Ryan Williams <ryan@runsascoded.com>
2020-12-17 23:06:34 -0500

    preserve known chunks in rechunk() when NaNs passed

--- dask/array/rechunk.py
+++ dask/array/rechunk.py
@@ -128,6 +128,13 @@ def _old_to_new(old_chunks, new_chunks):
       [(2, slice(5, 10, None))],
       [(3, slice(0, 10, None)), (4, slice(0, 10, None))]]]
     """
+    # if new_chunks is full of NaNs on a dimension where the existing chunks are known, pass through the existing
+    # chunk-sizes on that dimension
+    new_chunks = [
+        o if all(math.isnan(y) for y in n) and not any(math.isnan(x) for x in o) else n
+        for o, n in zip(old_chunks, new_chunks)
+    ]
+
     old_known = [x for x in old_chunks if not any(math.isnan(y) for y in x)]
     new_known = [x for x in new_chunks if not any(math.isnan(y) for y in x)]
 
