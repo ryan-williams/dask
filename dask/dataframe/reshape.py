350ea02f62cd010e1be427b585bdb0994e127cb6
Ryan Williams <ryan@runsascoded.com>
2020-12-18 23:00:42 -0500

    preserve_partition_sizes: numeric, get_dummies

--- dask/dataframe/reshape.py
+++ dask/dataframe/reshape.py
@@ -172,6 +172,7 @@ def get_dummies(
         sparse=sparse,
         drop_first=drop_first,
         meta=meta,
+        preserve_partition_sizes=True,
         dtype=dtype,
         **kwargs
     )
